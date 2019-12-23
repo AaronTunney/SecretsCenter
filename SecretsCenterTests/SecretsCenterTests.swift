1e2ee9bd9b036816b032fa78d84fb31ecdfb202f
Aaron Tunney <aaron.tunney@gmail.com>
2019-12-23 19:24:57 +0000

    Initial Commit

---
+++ SecretsCenterTests/SecretsCenterTests.swift
@@ -0,0 +1,34 @@
+//
+//  SecretsCenterTests.swift
+//  SecretsCenterTests
+//
+//  Created by Tunney, Aaron (ELS) on 23/12/2019.
+//  Copyright © 2019 Aaron Tunney. All rights reserved.
+//
+
+import XCTest
+@testable import SecretsCenter
+
+class SecretsCenterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
